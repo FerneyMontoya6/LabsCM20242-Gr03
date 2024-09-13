2b45890b7ba29a1eecae51e589497e91137d38a0
Ferney <ferney.montoyag@gmail.com>
2024-09-13 18:53:18 -0500

    :sparkles: ContactDataActivity layout completed

--- Lab1-UI/src/main/java/com/example/labscm20242_gr03/screens/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/com/example/labscm20242_gr03/screens/ContactDataActivity.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Call
 import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.labscm20242_gr03.navigation.AppScreens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,6 +24,7 @@ fun ContactDataActivity(navController: NavController) {
     var inputPhone by remember { mutableStateOf("") }
     var inputEmail by remember { mutableStateOf("") }
     var inputCountry by remember { mutableStateOf("") }
+    var inputAddress by remember { mutableStateOf("") }
 
     val latinAmericanCountries = listOf(
         "Argentina", "Bolivia", "Brasil", "Chile", "Colombia", "Costa Rica",
@@ -72,7 +77,10 @@ fun ContactDataActivity(navController: NavController) {
                 onCountrySuggestionClick = { country ->
                     inputCountry = country
                     countrySuggestions = emptyList()
-                }
+                },
+                navController = navController,
+                inputAddress = inputAddress,
+                onAddressChange = { newValue -> inputAddress = newValue }
             )
         }
     }
@@ -88,7 +96,10 @@ fun ContactPortraitLayout(
     inputCountry: String,
     countrySuggestions: List<String>,
     onCountryChange: (String) -> Unit,
-    onCountrySuggestionClick: (String) -> Unit
+    onCountrySuggestionClick: (String) -> Unit,
+    navController: NavController,
+    inputAddress: String,
+    onAddressChange: (String) -> Unit
 ) {
     var expanded by remember { mutableStateOf(false) }
 
@@ -191,6 +202,105 @@ fun ContactPortraitLayout(
                 }
             }
         }
+
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(bottom = 20.dp)
+                    .padding(end = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    Icons.Rounded.LocationOn,
+                    contentDescription = "Location icon",
+                    modifier = Modifier
+                        .padding(end = 6.dp)
+                        .size(36.dp)
+                )
+
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = { expanded = !expanded },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentSize(Alignment.TopStart)
+                ) {
+                    TextField(
+                        value = inputCountry,
+                        onValueChange = {
+                            onCountryChange(it)
+                            expanded = true
+                        },
+                        label = { Text("Ciudad") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .menuAnchor()
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = expanded && countrySuggestions.isNotEmpty(),
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        countrySuggestions.forEach { suggestion ->
+                            DropdownMenuItem(
+                                text = { Text(suggestion) },
+                                onClick = {
+                                    onCountrySuggestionClick(suggestion)
+                                    expanded = false
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                Modifier
+                    .fillMaxWidth(0.85f)
+                    .padding(bottom = 10.dp),
+                horizontalArrangement = Arrangement.End,
+            ) {
+                Text("Dirección")
+            }
+            Row(
+                modifier = Modifier.padding(bottom = 20.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    Icons.Rounded.Home,
+                    contentDescription = "Home icon",
+                    modifier = Modifier
+                        .padding(end = 6.dp)
+                        .size(36.dp)
+                )
+                TextField(
+                    value = inputAddress,
+                    onValueChange = onAddressChange,
+                    label = { Text("Carrera 51") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+                )
+            }
+        }
+
+        Row (
+            Modifier
+                .fillMaxWidth(0.85f),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Button(onClick = {
+                navController.navigate(route = AppScreens.PersonalDataActivity.route)
+            }) {
+                Text("Siguiente")
+            }
+        }
+
     }
 }
 
